063facdab277439563f0c7f545b38ab338be8abd
Alyson P Seidman <aseidma21@ccs.local>
2020-01-30 15:34:41 -0500

    adding intermediate arduino assignments

---
+++ Motor_control.ino
@@ -0,0 +1,25 @@
+const int potpin = A5;
+const int diodepin = A4;
+int value;
+int speed;
+void setup()
+{
+	pinMode(potpin, INPUT);
+	pinMode(diodepin, OUTPUT);
+	
+}
+
+void loop()
+{
+ value = analogRead(potpin);      //Read and save analog value from potentiometer
+ //speed = map(value, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
+  analogWrite(diodepin, value);      	//Send PWM value to led
+  delay(100);	
+
+	delay(500);	
+}
+
+  
+
+
+
