26feba579d3eaa7735270a88085b2312090f51f5
aseidma21 <59747622+aseidma21@users.noreply.github.com>
2020-02-10 15:20:03 -0500

    Update LCD hello world.ino

--- Hello LCD/LCD hello world.ino
+++ Hello LCD/LCD hello world.ino
@@ -1,11 +1,11 @@
 
-#include <LiquidCrystal.h>
+#include <LiquidCrystal.h> //Includes LCD code Library
 
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //These are the pins for RS, EN, DB4, DB5, DB6, and DB7(respectively)
 
 void setup(){
 	lcd.begin(16, 2);
-	lcd.setCursor(0,0);
+	lcd.setCursor(0,0); //sets LCD to start printing at the first line and first collumn
 	lcd.print("hello, world!"); //print a message "hello world" on the lcd
 }
 void loop(){
