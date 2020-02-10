0ab62234ef2bf85da802b389fd2bb06bb6c7b4f5
aseidma21 <59747622+aseidma21@users.noreply.github.com>
2020-02-10 15:18:12 -0500

    Update LCD hello world.ino

--- Hello LCD/LCD hello world.ino
+++ Hello LCD/LCD hello world.ino
@@ -6,7 +6,7 @@ LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 void setup(){
 	lcd.begin(16, 2);
 	lcd.setCursor(0,0);
-	lcd.print("hello, world!"); //print a message on the lcd
+	lcd.print("hello, world!"); //print a message "hello world" on the lcd
 }
 void loop(){
 	lcd.setCursor(0, 1); //sets the cursor to column 0 line 1
